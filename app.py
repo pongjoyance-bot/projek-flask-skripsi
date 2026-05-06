cefd99d02fca1ffec8db6759cba628462baf6983
Joy Linggi Alo <joy@gmail.com>
2026-05-06 10:16:58 +0800

    first commit

--- app.py
+++ app.py
@@ -29,10 +29,11 @@ import os
 
 #  Koneksi ke Database
 db = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="",  
-    database="aktivasi_acount",  
+    host=os.getenv("DB_HOST"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    database=os.getenv("DB_NAME"),
+    port=int(os.getenv("DB_PORT", 3306))
 )
 
 
